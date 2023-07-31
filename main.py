c99e766d920fcb271ef80492287aa6f7677b14cc
Gerten-Pilv7 <gerten.pilv7@gmail.com>
2023-07-31 19:33:04 +0300

    Palli liikumine parandatud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import sys
 
 pygame.init()
 
@@ -8,7 +9,12 @@ x_ekraan = 800
 y_ekraan = 600
 ekraan = pygame.display.set_mode([x_ekraan, y_ekraan])
 pygame.display.set_caption("Ping Pong | Gerten Pilv'")
-
+suund_x = 2 if random.randint(0, 1) == 0.1 else -0.1
+suund_y = 1 if random.randint(0, 1) == 0.1 else -0.1
+pall_x = 420
+pall_y = 300
+char1_skoor_lugeja = 0
+char2_skoor_lugeja = 0
 def mäng():
     # tausta tegemine :p
     def taust():
@@ -32,40 +38,54 @@ def mäng():
 
     # mängija 2
     v2x = 0
-    mängija2x = 30
+    mängija2x = 45
     mängija2y = 100
     def char2(mängija2x, mängija2y):
         pygame.draw.rect(ekraan, [255, 255, 255], [x_ekraan - mängija2x, mängija2y, 12, 70], 0)
         pygame.draw.rect(ekraan, [173, 173, 173], [x_ekraan - mängija2x, mängija2y - 2, 14, 74], 3)
 
     # pall
-    pall_x = 420
-    pall_y = 300
-    suund_x = 2 if random.randint(0, 1) == 0.2 else -0.2
-    suund_y = 1 if random.randint(0, 1) == 0.2 else -0.2
     def pall():
-        global pall_x
-        global pall_y
-        global suund_x
-        global suund_y
+        global pall_y, pall_x, suund_y, suund_x, char2_skoor_lugeja, char1_skoor_lugeja
         pygame.draw.circle(ekraan, [255, 255, 255], [pall_x, pall_y], 12, 0)
         pygame.draw.circle(ekraan, [173, 173, 173], [pall_x, pall_y], 13, 3)
         if pall_y <= 0 or pall_y >= y_ekraan:
             suund_y *= -1
-        if int(pall_x) == int(mängija1x) + 12 and int(pall_y) in range(int(mängija1y), int(mängija1y) + 70):
+        elif int(pall_x) == int(mängija1x) + 12 and int(pall_y) in range(int(mängija1y) - 10, int(mängija1y) + 70):
             suund_x *= -1
-        if int(pall_x) == int(x_ekraan - mängija2x) and int(pall_y) in range(int(mängija2y), int(mängija2y) + 70):
+        elif int(pall_x) == int(x_ekraan - mängija2x) and int(pall_y) in range(int(mängija2y) - 10, int(mängija2y) + 70):
             suund_x *= -1
+        if pall_x <= 20:
+            suund_x = 2 if random.randint(0, 1) == 0.1 else -0.1
+            suund_y = 1 if random.randint(0, 1) == 0.1 else -0.1
+            pall_x = 420
+            pall_y = 300
+            char2_skoor_lugeja += 1
+        elif pall_x >= 770:
+            suund_x = 2 if random.randint(0, 1) == 0 else -0.1
+            suund_y = 1 if random.randint(0, 1) == 0 else -0.1
+            pall_x = 420
+            pall_y = 300
+            char1_skoor_lugeja += 1
         pall_x += suund_x
         pall_y += suund_y
         print("see on x:", pall_x)
         print("see on y:", pall_y)
 
-    running = True
-    while running:
+    def char1_skoor(skoor):
+        font = pygame.font.Font('font.ttf', 70)
+        tekst_pildina = font.render(str(skoor), 1, [255, 255, 255])
+        ekraan.blit(tekst_pildina, [350, 20])
+
+    def char2_skoor(skoor):
+        font = pygame.font.Font('font.ttf', 70)
+        tekst_pildina = font.render(str(skoor), 1, [255, 255, 255])
+        ekraan.blit(tekst_pildina, [470, 20])
+
+    while True:
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
-                running = False
+                sys.exit()
             elif i.type == pygame.KEYDOWN:
                 if i.key == pygame.K_w:
                     v1x = -0.5
@@ -83,6 +103,8 @@ def mäng():
         mängija1y += v1x
         mängija2y += v2x
         taust()
+        char1_skoor(char1_skoor_lugeja)
+        char2_skoor(char1_skoor_lugeja)
         pall()
         char1(mängija1x, mängija1y)
         char2(mängija2x, mängija2y)
@@ -118,11 +140,10 @@ def tiitel():
         ekraan.blit(tekst_pildina, [x, y])
 
     taust = pygame.image.load("taust.png")
-    running = True
-    while running:
+    while True:
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
-                running = False
+                sys.exit()
             if i.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
                 if pos[0] in range(270, 506) and pos[1] in range(247, 335):
